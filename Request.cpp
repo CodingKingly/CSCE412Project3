bf34d0b25b4990cb23c1143fc64b0e8d09a7fcc9
“abelasteway” <astewayabel@gmail.com>
2026-07-10 21:18:40 -0500

    requests made

--- Request.cpp
+++ Request.cpp
@@ -1,6 +1,35 @@
-struct Request {
-    int IPin;
-    int IPout;
-    int totalTime;
-    char jobType;
-};
+#include "Request.h"
+
+
+
+Request makearandomRequest()
+{   
+    //for randomIPin and randomIPout
+    std::string randomIPin = "";
+    std::string randomIPout = "";
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(0, 255);
+
+    randomIPin += to_string(distrib(gen)) + "." + to_string(distrib(gen)) + "." + to_string(distrib(gen)) + "." + to_string(distrib(gen));
+    randomIPout += to_string(distrib(gen)) + "." + to_string(distrib(gen)) + "." + to_string(distrib(gen)) + "." + to_string(distrib(gen));
+
+    // Random Completion time
+    uniform_int_distribution<> randomTimeDistrib(0,100);
+
+    int randomTotalTime = randomTimeDistrib(gen);
+
+    //random jobtype 
+    char randomJobType;
+    if(rand() % 2 == 0){
+        randomJobType = 'p';
+    }
+    else{
+        randomJobType = 's';
+    }
+
+    struct Request RandomRequest = {randomIPin, randomIPout,randomTotalTime, randomJobType };
+    
+    return RandomRequest;
+}
